4c877efedbb90230ed9683a629346a0a0a460cd2
Nguyễn Duy Khương <kn134646@gmail.com>
2023-07-23 14:07:03 +0700

    update

--- poly/src/main/java/com/example/demo/controller/StudentController.java
+++ poly/src/main/java/com/example/demo/controller/StudentController.java
@@ -13,22 +13,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
     @GetMapping
     private String index(Model model,
-                         @RequestParam("index") Integer index) throws IOException {
+                         @RequestParam("index") Optional<Integer> index) throws IOException {
         //Đọc danh sách sinh viên từ file
         ObjectMapper mapper = new ObjectMapper();
-        File path = ResourceUtils.getFile("classpath:com/example/demo/bean/students.json");
+        File path = ResourceUtils.getFile("C:\\Users\\kn134\\Downloads\\Java-6-test\\poly\\src\\main\\java\\com\\example\\demo\\bean\\students.json");
         TypeReference<List<Student>> typeReference = new TypeReference<>() {
         };
         List<Student> students = mapper.readValue(path, typeReference);
-        int i = index.describeConstable().orElse(0);
-        model.addAttribute("n",i);
-        model.addAttribute("sv",students.get(i));
+        int i = index.orElse(0);
+        model.addAttribute("n", i);
+        model.addAttribute("sv", students.get(i));
         return "scope/student";
     }
 }
