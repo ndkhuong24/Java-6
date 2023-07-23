a2d481f357d85c637ae0997650816774678dad7c
Nguyễn Duy Khương <kn134646@gmail.com>
2023-07-23 13:02:21 +0700

    update

--- poly/src/main/java/com/example/demo/controller/ScopeController.java
+++ poly/src/main/java/com/example/demo/controller/ScopeController.java
@@ -1,5 +1,11 @@
 package com.example.demo.controller;
 
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/scope")
 public class ScopeController {
+    @Autowired
+    HttpServletRequest request;
+
+    @Autowired
+    HttpSession session;
+
+    @Autowired
+    ServletContext servletContext;
+
     @GetMapping
     public String index(Model model){
         model.addAttribute("a","I am in Model");
+        request.setAttribute("b","I am in Request Scope");
+        session.setAttribute("c","I am in Session Scope");
+        servletContext.setAttribute("d","I am in Application Scope");
         return "scope/index";
     }
 }
