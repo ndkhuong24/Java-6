f9a724db224698b8cc709d9d8ccdb457255b1638
Nguyễn Duy Khương <kn134646@gmail.com>
2023-07-28 18:10:59 +0700

    up

---
+++ DataBinding-Validation/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.bean.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+    @GetMapping("/form")
+    public String form(Model model){
+        Student student = new Student();
+        student.setFullname("Nguyễn Văn Tèo");
+        student.setCountry("VN");
+        student.setGender(true);
+        model.addAttribute("student",student);
+        return "student/form";
+    }
+    @PostMapping("/save")
+    public String save(@ModelAttribute("student")Student student){
+        return "student/success";
+    }
+}
