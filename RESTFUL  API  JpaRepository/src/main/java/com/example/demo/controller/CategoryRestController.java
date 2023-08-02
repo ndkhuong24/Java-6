0e3dacb400fe4a4efb7aab25c68ef286fec80c9c
Nguyễn Duy Khương <kn134646@gmail.com>
2023-08-02 22:11:32 +0700

    update

--- RESTFUL  API  JpaRepository/src/main/java/com/example/demo/controller/CategoryRestController.java
+++ RESTFUL  API  JpaRepository/src/main/java/com/example/demo/controller/CategoryRestController.java
@@ -5,10 +5,7 @@ import com.example.demo.repository.CategoryReposiroty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,32 @@ public class CategoryRestController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(categoryReposiroty.findById(id).get());
+    }
+
+    @PostMapping("/rest/categories")
+    public ResponseEntity<Category> post(@RequestBody Category category) {
+        if (categoryReposiroty.existsById(category.getId())) {
+            return ResponseEntity.badRequest().build();
+        }
+        categoryReposiroty.save(category);
+        return ResponseEntity.ok(category);
+    }
+
+    @PutMapping("/rest/categories/{id}")
+    public ResponseEntity<Category> put(@PathVariable("id") Long id, @RequestBody Category category) {
+        if (!categoryReposiroty.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        categoryReposiroty.save(category);
+        return ResponseEntity.ok(category);
+    }
+
+    @DeleteMapping("/rest/categories/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        if (!categoryReposiroty.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        categoryReposiroty.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
