debf609903acbb9b5058fa8839a8d18bc78a84f8
Nguyễn Duy Khương <kn134646@gmail.com>
2023-08-06 17:23:07 +0700

    update

---
+++ SOF306-Test-1/src/main/java/com/example/demo/bean/HangKhachHang.java
@@ -0,0 +1,39 @@
+package com.example.demo.bean;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "hangkhachhang")
+public class HangKhachHang {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mahang")
+    private Integer maHang;
+
+    @Column(name = "tenhang")
+    private String tenHang;
+
+    @Column(name = "mota")
+    private String moTa;
+
+    @Column(name = "diemtoithieu")
+    private Integer diemToiThieu;
+
+    @Column(name = "trangthai")
+    private Integer trangThai;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "hangKhachHang")
+    private List<KhachHang> khachHang;
+}
---
+++ SOF306-Test-1/src/main/java/com/example/demo/bean/KhachHang.java
@@ -0,0 +1,61 @@
+package com.example.demo.bean;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "khachhang")
+public class KhachHang {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "makhachhang")
+    private BigDecimal maKhachHang;
+
+    @Column(name = "tenkhachhang")
+    private String tenKhachHang;
+
+    @Column(name = "sinhnhat")
+    private Date sinhNhat;
+
+    @Column(name = "sodienthoai")
+    private String soDienThoai;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "gioitinh")
+    private Boolean gioiTinh;
+
+    @Column(name = "chungminhthu")
+    private String chungMinhThu;
+
+    @Column(name = "socancuoc")
+    private String soCanCuoc;
+
+    @Column(name = "anhdaidien")
+    private String anhDaiDien;
+
+    @Column(name = "trangthai")
+    private Integer trangThai;
+
+    @Column(name = "diemtichluy")
+    private Integer diemTichLuy;
+
+    @ManyToOne
+    @JoinColumn(name = "hangkhachhang")
+    private HangKhachHang hangKhachHang;
+
+    @OneToOne
+    @JoinColumn(name = "nguoigioithieu")
+    private KhachHang khachHang;
+}
