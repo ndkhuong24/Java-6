814a96b89e55276847030709f79d63faa94e2a1a
Nguyễn Duy Khương <kn134646@gmail.com>
2023-08-06 10:35:52 +0700

    update

--- Basic Spring Security/src/main/java/com/example/demo/config/MySecurityConfiguration.java
+++ Basic Spring Security/src/main/java/com/example/demo/config/MySecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,11 +29,36 @@ public class MySecurityConfiguration {
 
         PasswordEncoder encoder = passwordEncoder();
 
+        List<UserDetails> users = new ArrayList<>();
+
         UserDetails user = User.builder().
                 username("sa").
                 password(encoder.encode("123")).
                 build();
+        users.add(user);
 
-        return new InMemoryUserDetailsManager(user);
+        UserDetails admin = User.builder().
+                username("admin").
+                password(encoder.encode("123")).
+                roles("ADMIN").
+                build();
+        users.add(admin);
+
+        return new InMemoryUserDetailsManager(users);
+    }
+
+    @Bean
+    public SecurityFilterChain authorization(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.
+                authorizeHttpRequests(req -> req
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().permitAll()
+                ).formLogin(login -> login
+                                .loginProcessingUrl("/login")
+                ).logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                )
+        ;
+        return httpSecurity.build();
     }
 }
