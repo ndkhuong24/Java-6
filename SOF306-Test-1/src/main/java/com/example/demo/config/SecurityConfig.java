3fd2801381f5004cce8162866628314400b8018d
Nguyễn Duy Khương <kn134646@gmail.com>
2023-08-07 18:27:47 +0700

    update

--- SOF306-Test-1/src/main/java/com/example/demo/config/SecurityConfig.java
+++ SOF306-Test-1/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,15 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
