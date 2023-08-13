d01ff38e1f81eb53a42087ccab5eabef939cb1b2
Nguyễn Duy Khương <kn134646@gmail.com>
2023-08-13 17:05:39 +0700

    update

--- SOF306 Test 1/src/main/java/com/example/demo/config/SecurityConfig.java
+++ SOF306 Test 1/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -25,8 +27,22 @@ public class SecurityConfig {
         UserDetails user = User.builder()
                 .username("sa")
                 .password(encoder.encode("123"))
+                .roles("ADMIN")
                 .build();
-
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain authorization(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers("/khach-hang/**")
+                        .hasRole("ADMIN")
+                        .anyRequest().permitAll())
+                .formLogin(login -> login
+                        .loginProcessingUrl("/login"))
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login"));
+        return httpSecurity.build();
     }
 }
