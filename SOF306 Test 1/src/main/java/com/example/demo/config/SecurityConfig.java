83fd5f1b56df770d951d94e3178ce940f6e3a99c
Nguyễn Duy Khương <kn134646@gmail.com>
2023-08-13 16:19:12 +0700

    update

--- SOF306 Test 1/src/main/java/com/example/demo/config/SecurityConfig.java
+++ SOF306 Test 1/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,22 +1,32 @@
 package com.example.demo.config;
 
-//@Configuration
-//@EnableWebSecurity
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public UserDetailsService authentication() {
-//        PasswordEncoder encoder = passwordEncoder();
-//
-//        UserDetails user = User.builder()
-//                .username("sa")
-//                .password(encoder.encode("123"))
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService authentication() {
+        PasswordEncoder encoder = passwordEncoder();
+
+        UserDetails user = User.builder()
+                .username("sa")
+                .password(encoder.encode("123"))
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
