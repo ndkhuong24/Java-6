1766cec04e60f2c746fe0ca8230b8c20db809e81
Nguyễn Duy Khương <kn134646@gmail.com>
2023-08-14 10:04:32 +0700

    update

--- SOF306 Test 1/src/main/java/com/example/demo/service/impl/IHangKhachHangService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.bean.HangKhachHang;
-
-import java.util.List;
-
-public interface IHangKhachHangService {
-    List<HangKhachHang> getAll();
-}
